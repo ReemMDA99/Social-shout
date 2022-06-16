d8efe9e37b5d944052ac2034102a13aa8401b2ee
Reem Ahmed <reem.mda0909@gmail.com>
2022-06-16 13:06:03 -0600

    create and setup reaction schema in thought file

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,45 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
+
+// Reaction schema
+const ReactionSchema = new Schema({
+    // reactionId, reactionBody, username, createdAt
+    reactionId: {
+        // Use Mongoose's ObjectId data type
+        type: Schema.Types.ObjectId,
+        //Default value is set to a new ObjectId
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        // String, Required, 280 character maximum
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        // String, Required
+        type: String,
+        required: true
+    },
+    createdAt: {
+        // Date
+        type: Date,
+        // Set default value to the current timestamp
+        default: Date.now,
+        // Use a getter method to format the timestamp on query
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+// ThoughtSchema
 const ThoughtSchema = new Schema({
     // thoughtSchema includes thoughtText, createdAt, username, reactions
     thoughtText: {
