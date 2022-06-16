a6cfc4098d8bc7130515a7ed7e4f6b80818ee063
Reem Ahmed <reem.mda0909@gmail.com>
2022-06-16 14:54:48 -0600

    create user controllers

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -24,36 +24,93 @@ const userController = {
     },
     // get single user by id
     getUserById({params}, res) {
+        User.findOne({_id: params.id})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message:'No user found with this id!'})
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
 
+        });
+
+    
     },
 
     // add user
-    addUser({ body } , res) {
+    addUser({ body }, res) {
+        User.create(body) 
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.json(err));
 
     },
 
     // Update user by id
-    updateUser({params, body}, res) {
-
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate(
+            {_id: params.id}, 
+            body,
+            {new: true, runValidators: true}
+            ).then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404)
+                    .json({message: 'No user was found with this id!'});
+                    return;
+                }
+                res.json(dbUserData);
+            }).catch(err => res.json(err)
+        );
     },
 
-// Delete user and users associated thoughts
+    // Delete user and users associated thoughts
     deleteUser({params}, res) {
+        User.findOneAndDelete ({
+            _id: params.id
+        })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404)
+                .json({ message: 'No user found with this id!'})
+                return;
+            }
+            res.json(dbUserData);
+        }).then(dbUserData => {
+            User.updateMany(
+                {_id: {$in: dbUserData.friends}},
+                {$pull: {friends: params.userId}}
+            )
+        }).then(() => {
+            Thoughts.deleteMany({
+                username: dbUserData.username
+            })
+        }).then(() => {
+            res.json({message: 'Deleted user'});
+        }).catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        })
+    },
 
-    }
-
- // add friend
+    // add friend
     addFriend({params}, res) {
 
     },
 
- // delete friend
+    // delete friend
     deleteFriend({params}, res) {
     }
-
-}
-
-
-
+};
 
 module.exports = userController
