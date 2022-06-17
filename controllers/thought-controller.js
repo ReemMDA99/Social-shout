8f6c975e692195c3e437936a329fc3a18a8c5946
Reem Ahmed <reem.mda0909@gmail.com>
2022-06-16 18:32:22 -0600

    add reactions and delete reaction functions to thoughts routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -21,7 +21,7 @@ const thoughtController = {
 
     // get single thought by _id
     getThoughtById({ params}, res) {
-        Thought.findOne({_id: params:id})
+        Thought.findOne({_id: params.thoughtId})
         .populate({ path: 'reactions', select: '-__v'})
         .select({'-__v'})
         .sort({_id:-1})
@@ -54,11 +54,95 @@ const thoughtController = {
 
             }
             res.json(dbThoughtData);
-        }).catch(err=> res.json(err));
+        }).catch(err=> {
+            console.log(err);
+            res.status(500).json(err);
+        });
     },
     // update thought by id
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId }, 
+            body, 
+            { new: true, runValidators: true }
+        ).then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thoughts found with that id!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err=> {
+            console.log(err);
+            res.status(500).json(err)
+        });
+    },
+    
     // delete thought
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtId })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+              res.status(404).json({ message: 'No thoughts found with that id!' });
+              return;
+            }
+            return User.findOneAndUpdate(
+                { _id: params.userId },
+                { $pull: { thoughts: params.Id } },
+                { new: true }
+            )
+        }).then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No User found with this id!' });
+              return;
+            }
+            res.json(dbUserData);
+        }).catch(err=> {
+            console.log(err);
+            res.status(500).json(err)
+        });
+    },
+
     //create reactions
-     // Delete a reaction
-}
+    addReaction({params, body}, res){
+        Thought.findOneAndUpdate(
+            {_id: params.thoughtId},
+            {$push: {reactions: body}},
+            { new: true, runValidators: true }
+        )
+        .populate({path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtData => {
+            if (!dBThoughtData) {
+                res.status(404).json({ message: 'Incorrect reaction data!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        }).catch(err=> {
+            console.log(err);
+            res.status(500).json(err)
+        });
+    },
+
+    // Delete a reaction
+    deleteReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+          { _id: params.thoughtId },
+          { $pull: { reactions: { reactionId: params.reactionId } } },
+          { new: true }
+        ).then(dbThoughtData => {
+            if (!dbThoughtData) {
+              res.status(404).json({ message: 'Nope!'});
+              return;
+            }
+           res.json(dbThoughtData);
+        }).catch(err=> {
+            console.log(err);
+            res.status(500).json(err)
+        });
+    }
+    
+
+};
+
 module.exports = thoughtController
